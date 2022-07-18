efed33e8691df1b2c00d4b51350b04498dd63cdb
Khalid HAMDANI <khalidhamdani25@gmail.com>
2022-07-18 14:51:37 +0200

    changes

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -23,6 +23,57 @@ class OrderController extends Controller
 
         // total is without 50 dh of delivery
 
+
+        // [
+        //     "name" => "sdfqsf"
+        //     "phone" => "0600000000"
+        //     "city" => "agadir"
+        //     "terms" => true
+        //     "products" => array:4 [
+        //       0 => array:8 [
+        //         "id" => "bc9a19dfzezs"
+        //         "name" => "PACK OLIVES"
+        //         "image" => "https://picsum.photos/seed/picsum/200/300"
+        //         "description" => null
+        //         "price" => 1995
+        //         "option_id" => null
+        //         "option" => null
+        //         "quantity" => 1
+        //       ]
+        //       1 => array:8 [
+        //         "id" => "bca194454f"
+        //         "name" => "SELLOU HOME MADE"
+        //         "image" => "https://picsum.photos/seed/picsum/200/300"
+        //         "description" => null
+        //         "price" => 80
+        //         "option_id" => "bca194454f-250-G"
+        //         "option" => "250 G"
+        //         "quantity" => 2
+        //       ]
+        //       2 => array:8 [
+        //         "id" => "bca194454f"
+        //         "name" => "SELLOU HOME MADE"
+        //         "image" => "https://picsum.photos/seed/picsum/200/300"
+        //         "description" => null
+        //         "price" => 100
+        //         "option_id" => "bca194454f-500-G"
+        //         "option" => "500 G"
+        //         "quantity" => 2
+        //       ]
+        //       3 => array:8 [
+        //         "id" => "bca194454f"
+        //         "name" => "SELLOU HOME MADE"
+        //         "image" => "https://picsum.photos/seed/picsum/200/300"
+        //         "description" => null
+        //         "price" => 120
+        //         "option_id" => "bca194454f-1-KG"
+        //         "option" => "1 KG"
+        //         "quantity" => 1
+        //       ]
+        //     ]
+        //     "total" => 2475
+        //   ]
+
         $ord_num = random_int(100000, 999999);
         $values = [
             $ord_num, 
@@ -30,7 +81,7 @@ class OrderController extends Controller
             $request->phone, 
             $request->city, 
             'Argan 50DH, Amlou 12DH', 
-            '2344 MAD', 
+            $request->total. ' + 50 MAD', //'2344 MAD',  
             date('Y-m-d H:i:s')
         ];
 
